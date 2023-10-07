45fc575620a4648b86170652ff6e78b2f88b44ea
Arjun Kumar <kumararjundamai1999@gmail.com>
2023-10-07 21:25:05 +0530

    Minor changes

--- WelcomePage.py
+++ WelcomePage.py
@@ -1,19 +1,44 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
 
-root = Tk()
+root = tk.Tk()
 root.title("WELCOME")
 root.geometry("500x500")  # Size of my window
-root.resizable(width=False,height=False)
+# root.resizable(width=False, height=False)
+root.rowconfigure(0)
+root.columnconfigure(0)
 
-mainframe = ttk.Frame(root)
+mainframe = tk.Frame(root)
 mainframe.grid(row=0, column=0)
-root.rowconfigure(0, weight=1)
-root.columnconfigure(0, weight=1)
 
-ttk.Button(mainframe, text="Click to generate pair of keys", width=50).grid(
-    row=0, column=0, sticky=W)
-ttk.Button(mainframe, text="Click to secure your file", width=50).grid(
-    row=1, column=0, sticky=W)
+introduction="Hi, welcome to my small tool which will provide you method to encrypt or decrypt your data based on Elliptical Curve Cryptography. I hope you will like it. ☺️Keep smiling ☺️"
+
+intro_lb=tk.Label(mainframe,
+                      text=introduction,
+                      border=10)
+intro_lb.grid(row=1,column=0)
+
+gen_btn = tk.Button(
+    mainframe,
+    text="CLICK HERE TO GENERATE THE KEY PAIR",
+    cursor="pirate",
+    width=50,
+    background='Black',
+    foreground='White',    
+    border=10)
+gen_btn.grid(row=2, column=0)
+
+seperator=tk.Label(mainframe,
+                   text="")
+seperator.grid(row=3,column=0)
+
+gen_btn = tk.Button(
+    mainframe,
+    text="CLICK HERE TO SECURE YOUR FILE",
+    cursor="pirate",
+    width=50,
+    background='Black',
+    foreground='White',    
+    border=10)
+gen_btn.grid(row=4, column=0)
 
 root.mainloop()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,3 @@
-root.geometry("500x500")  # Size of my window
+text = tk.Label(mainframe, height=10, width=20)
+# text.insert(1.0, "This is a long introduction paragraph.")
+# text.grid(row=0, column=0)
